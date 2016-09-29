3eb372e5f8cc8579db80ae01ee8eea1d7f147b78
xavier <exa.dev@outlook.com>
2016-09-29 23:08:14 +0200

    French version of the studet list

--- js/students.js
+++ js/students.js
@@ -1,4 +1,4 @@
-//This is the latin version
+//This is the french version
 
 var students = [ 
   { 
@@ -8,31 +8,31 @@ var students = [
     points: 14730
   },
   {
-    name: 'Juan',
+    name: 'Julian',
     track: 'iOS Development with Swift',
     achievements: '175',
     points: '16375'
   },
   {
-    name: 'Julian',
+    name: 'François',
     track: 'Android',
     achievements: '175',
     points: '16375'
   },
   {
-    name: 'Marcelo',
+    name: 'Marcel',
     track: 'PHP Development',
     achievements: '55',
     points: '2025'
   },
   {
-    name: 'Juanito',
+    name: 'Fabien',
     track: 'Learn WordPress',
     achievements: '40',
     points: '1950'
   },
   {
-    name: 'Teresa',
+    name: 'Fabienne',
     track: 'Rails Development',
     achievements: '5',
     points: '350'
